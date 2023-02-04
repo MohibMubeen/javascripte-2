32d69cd180d238aec0e0ed9cb2dc35ffbe1fab6a
ComTech <mohibmubeen058@gmail.com>
2023-02-04 14:53:53 +0500

    first  commit

---
+++ mohib.js
@@ -0,0 +1,16 @@
+var age = "20 years old"
+alert(age)
+var visit = "You have visit the site 20 times"
+alert(visit)
+var birthYear = "My birth year is 1990 <br> Data type of mt declared varibale is number"
+document.write(birthYear)
+var f = " Name:John Doe"
+console.log(f)
+var p = "product tittle:T-shirt"
+console.log(p)
+var y = "order:T-shirt5"
+console.log(y)
+var head = "<center><h1>XYZ CLOTHING STORE</h1></cemter>"
+document.write(head)
+var t = "John Doe order 5-T-shirt on xyz clothing store"
+document.write(t)
---
+++ tech.js
@@ -0,0 +1,32 @@
+// var main()
+// {
+//     var firstName = "none"
+//     var lastName = "none"
+//     var age = "none"
+// }
+
+// //   var so = "<h1> ILLEGAL VARIABLE </h1>"
+//   var var = "1"
+//   var 2hi = "00"
+//   var @jk = "87"
+//   var FirstNAME = "54"
+//     var no-2 = "90"
+
+//   var re = "<h1>LEGAL VARIBLE</h1>"
+//   var name = "4"
+//   var firstName = "10"
+//   var last_name
+//   var no21 = "90"
+//   var $hi = "76"
+var ab = "<h1>QUESTION#3</h1>"
+document.write(ab)
+var bc = "<h2>1: Rules for js variable </h2>"
+document.write(bc)
+var fg = "<h2>2: Letter, Number, dollar signs and underscores </h2>"
+document.write(fg)
+var yt = "<h2>3: Letter, $, __ </h2>"
+document.write(yt)
+var er = "<h2>4: Sensitive </h2>"
+document.write(er)
+var hj = "<h2>5: Keywards </h2>"
+document.write(hj)
