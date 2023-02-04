63f44ffd22bc62a585ac667e760a0147c8c6b0d8
ComTech <mohibmubeen058@gmail.com>
2023-02-04 05:27:31 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var username = "1:Techcom"
+console.log(username)
+var b = "2:Muhammad Muhib"
+console.log(b)
+var messege = "<h1> Hello world </h1>"
+   document.write(messege)
+   var d = "M.Anas"
+   alert(d)
+   var e = "Age=15"
+   alert(e)
+   var f = "certificate of mobile application"
+   alert(f)
+   var g = "Pizza \n Pizz \n Piz \n Pi \n P"
+   alert(g)
+   var email = "mohibmubeen058@gmail.com"
+   alert(email)
+   var book = "A smarter way to learn JavaScript"
+    alert(book)
+    var write = "YAH!I can write HTML content through javascripte"
+    alert(write)
+    var a = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+    alert(a)
+console.log(a)
